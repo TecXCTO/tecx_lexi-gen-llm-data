01b28cb16d8fc0e9566885da0a0af056bba295b9
TecXCTO <cto.tecx@gmail.com>
2026-07-27 21:15:08 +0530

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -22,7 +22,8 @@ class Iterative_Nested_Word_Generator:
         if len(created_word[-1]) < self.ch_len:
           # self.iterative_nested_generator(created_word[-1])
           # created_word[-1] = self.iterative_nested_generator(created_word[-1])
-          created_word[-1].append(self.iterative_nested_generator(created_word[-1]))
+          # created_word[-1].append(self.iterative_nested_generator(created_word[-1]))
+          created_word[-1] += self.iterative_nested_generator(created_word[-1])
         #elif len(created_word) > 4: return
     else:
       return
