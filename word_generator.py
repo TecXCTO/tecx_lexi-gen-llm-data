f38cfa000752a40703a4fd770b7fc3b33aa79f02
RAH333 <142141323+RAH333@users.noreply.github.com>
2026-07-27 18:38:04 +0530

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -1,7 +1,16 @@
+import string
+
 class Iterative_nlNested_Word_Generator:
   def __init__(self):
     print("Iterative_nlNested_Word_Generator has been started.")
-    self.current_word = 
+    lowercase = string.ascii_lowercase          # a-z (26)
+    self.current_word = lowercase
     
   def iterative_nested_generator(self, given_word):
     gw = given_word
+    for lc in lowercase:
+      created_word = gw.append(lc)
+      if len(created_word) < 4:
+        self.iterative_nested_generator(created_word)
+    yield created_word
+    # return created_word
