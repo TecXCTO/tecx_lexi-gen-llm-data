994fd92f7357b6a88962a13c4266f870c2531fb8
TecXCTO <cto.tecx@gmail.com>
2026-07-27 20:56:42 +0530

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -14,6 +14,17 @@ class Iterative_Nested_Word_Generator:
     gw = given_word
     print(f"The given Word is = {gw}")
     created_word = []
+    if len(created_word[-1]) <4:
+      for lc in self.lowercase:
+      created_word += [gw + lc]
+      #created_word.append(gw + lc)
+      print(f"The Created Word is = {created_word}")
+      if len(created_word[-1]) <= 4:
+        self.iterative_nested_generator(created_word[-1])
+      #elif len(created_word) > 4: return
+    else:
+      return
+    """
     for lc in self.lowercase:
       created_word += [gw + lc]
       #created_word.append(gw + lc)
@@ -21,7 +32,9 @@ class Iterative_Nested_Word_Generator:
       if len(created_word[-1]) <= 4:
         self.iterative_nested_generator(created_word[-1])
       elif len(created_word) > 4: return
+      """
     # yield created_word
+    print(f"The Created Word is before return = {created_word}")
     return created_word
 if __name__ == "__main__":
   inwg = Iterative_Nested_Word_Generator()
